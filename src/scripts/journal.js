efbbc78a5d8ed7ba748fd0d8ee59ff3152f705b2
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-18 11:55:55 -0500

    Create sample entry and link js files to index

---
+++ src/scripts/journal.js
@@ -0,0 +1,9 @@
+const journalEntries = [
+  {
+    date: "10/18/2018",
+    title: "Setting up a JSON Server",
+    concepts: ["JSON Server", "HTTP Requests"],
+    entry: "We learned how to setup a JSON server and I can't wait to set it up on my daily journal.",
+    mood: "Excited"
+  }
+];
