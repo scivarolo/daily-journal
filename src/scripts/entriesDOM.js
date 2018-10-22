1a31ccb9c98f3d2660a59b4221490e67db3dff05
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-22 15:40:23 -0500

    create render object

--- src/scripts/entriesDOM.js
+++ src/scripts/entriesDOM.js
@@ -4,13 +4,26 @@
   Entries frag is appended to entriesWrapper in DOM.
 */
 
-function outputEntries(entries, wrapperEl) {
-  const entriesWrapper = document.querySelector(wrapperEl);
-  const entriesFrag = document.createDocumentFragment();
+// function outputEntries(entries, wrapperEl) {
+//   const entriesWrapper = document.querySelector(wrapperEl);
+//   const entriesFrag = document.createDocumentFragment();
 
-  entries.forEach((entry) => {
-    entriesFrag.appendChild(buildEntry(entry));
-  });
+//   entries.forEach((entry) => {
+//     entriesFrag.appendChild(buildEntry(entry));
+//   });
 
-  entriesWrapper.appendChild(entriesFrag);
-}
+//   entriesWrapper.appendChild(entriesFrag);
+// }
+
+const render = Object.create({
+  entries(entries, wrapperEl) {
+    const entriesWrapper = document.querySelector(wrapperEl);
+    const entriesFrag = document.createDocumentFragment();
+
+    entries.forEach((entry) => {
+      entriesFrag.appendChild(buildEntry(entry));
+    });
+
+    entriesWrapper.appendChild(entriesFrag);
+  }
+});
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -3,4 +3,4 @@
     Queries the API and outputs the entries into the DOM
 */
 
-entriesAPI.getJournalEntries().then(entries => outputEntries(entries, "#entries"))
+entriesAPI.getJournalEntries().then(entries => render.entries(entries, "#entries"))
