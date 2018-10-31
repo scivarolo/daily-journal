e4ce462b3afcf61631445d54f26e5742f5b4c4ca
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-31 14:54:34 -0500

    Set up submit event listener and make fields required

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,7 +1,28 @@
 /*
-    Main application logic
-    Queries the API and outputs the entries into the DOM
+  Main application logic
+  Queries the API and outputs the entries into the DOM
 */
 
 entriesAPI.getJournalEntries()
   .then(entries => render.entries(entries, "#entries"))
+
+/*
+  Event Listener to submit new entries from form
+  */
+
+document.querySelector(".submit-entry").addEventListener("click", (e) => {
+  let title = document.querySelector("#entryTitle").value
+  let content = document.querySelector("#entryContent").value
+  let date = document.querySelector("#entryDate").value
+  let concepts = document.querySelector("#entryConcepts").value
+  let mood = document.querySelector("#entryMood").value
+
+  let entry = {
+    title: title,
+    content: content,
+    date: date,
+    concepts: concepts.split(", "),
+    mood: mood
+  }
+  console.log(entry)
+})
