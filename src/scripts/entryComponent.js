077b812865327de66e193504bd2663825186b52d
Sebastian Civarolo <scivarolo@gmail.com>
2018-11-01 11:38:21 -0500

    Update build.element to accept attributes Object for more flexibility

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -3,35 +3,39 @@
 */
 
 const build = {
-  element(el, className, content, ...children) {
+
+  element(el, attrObj, content, ...children) {
     // HTML Element Factory
     let element = document.createElement(el)
-    element.className = className || null
+    for(let attr in attrObj) {
+      element.setAttribute(attr, attrObj[attr])
+    }
     element.textContent = content || null
     children.forEach((child) => {
       element.appendChild(child)
     })
     return element
   },
+
   entry(entry) {
     // Build entry component
-    let entryTitle = this.element("h2", "entry__title", entry.title)
-    let entryDate = this.element("h3", "entry__date", entry.date)
+    let entryTitle = this.element("h2", {class: "entry__title"}, entry.title)
+    let entryDate = this.element("h3", {class: "entry__date"}, entry.date)
 
     let concepts = []
     entry.concepts.forEach((concept) => {
-      concepts.push(this.element("span", "concept", concept))
+      concepts.push(this.element("span", {class: "concept"}, concept))
     })
-    let entryConcepts = this.element("div", "entry__concepts", null, ...concepts)
+    let entryConcepts = this.element("div", {class: "entry__concepts"}, null, ...concepts)
 
-    let entryHeader = this.element("header", "entry__header", null, entryTitle, entryDate, entryConcepts)
+    let entryHeader = this.element("header", {class: "entry__header"}, null, entryTitle, entryDate, entryConcepts)
 
-    let contentP = this.element("p", null, entry.content)
-    let entryContent = this.element("div", "entry__content", null, contentP)
+    let contentP = this.element("p", {}, entry.content)
+    let entryContent = this.element("div", {class: "entry__content"}, null, contentP)
 
-    let entryMood = this.element("div", "entry__mood", `Mood: ${entry.mood}`)
-    let entryFooter = this.element("footer", "entry__footer", null, entryMood)
+    let entryMood = this.element("div", {class: "entry__mood"}, `Mood: ${entry.mood}`)
+    let entryFooter = this.element("footer", {class: "entry__footer"}, null, entryMood)
 
-    return this.element("article", "entry", null, entryHeader, entryContent, entryFooter)
+    return this.element("article", {class: "entry"}, null, entryHeader, entryContent, entryFooter)
   }
 }
